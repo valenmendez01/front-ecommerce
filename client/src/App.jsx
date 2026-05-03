1467d4c2c1a1ee5f7d47c4abdc81f2d75fca0f96
valenmendez01 <mendezvalentin824@gmail.com>
2026-05-03 10:57:17 -0300

    tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 
-import './App.css'
-
 function App() {
 
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind configurado
+      </h1>
+    </div>
   )
 }
 
