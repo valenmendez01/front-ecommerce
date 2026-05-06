40c3ee4f34ee55adfc482cbd1f49115ee8fc1260
valenmendez01 <mendezvalentin824@gmail.com>
2026-05-06 15:42:13 -0300

    heroUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
+import { Button } from "@heroui/react"
 
 function App() {
 
   return (
     <div className="flex h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind configurado
-      </h1>
+      <div className="flex h-screen items-center justify-center bg-gray-100">
+        <Button color="primary" variant="flat">Hola desde HeroUI</Button>
+      </div>
     </div>
   )
 }
